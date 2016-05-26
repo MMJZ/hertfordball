30c0242d12b4d4869c2e001e4177cf484d6024f1
Michael Agius <mike_agius@hotmail.co.uk>
2016-05-26 22:57:31 +0100

    menu

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 function thing() {
     var navItems = document.getElementsByTagName("nav")[0].children[0].children;
 
-    for (var i = 0; i < navItems.length; i++) {
+    for (var i = 1; i < navItems.length; i++) {
         navItems[i].className = "show";
     }
 
